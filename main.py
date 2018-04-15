e75f6d26a6ce71c4f11f449dbae6cac21f12044a
Sandra Edwards <lilsan23@aol.com>
2018-04-15 18:53:30 -0500

    removed database

--- main.py
+++ main.py
@@ -1,50 +1,39 @@
+
 from flask import Flask, request, redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
-app.config['SQLALCHEMY_ECHO'] = True
-db = SQLAlchemy(app)
-
-
-class Task(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
-
-    def __init__(self, name):
-        self.name = name
-        self.completed = False
-
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    return render_template('newpost.html')  
+    
+    title_error = ""
+    post_error = ""
 
-    if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-        db.session.add(new_task)
-        db.session.commit()
+    if title == "":
+        title_error = "Enter title"
 
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html',title="Get It Done!", 
-        tasks=tasks, completed_tasks=completed_tasks)
+    if post == "":
+        post_error = "Enter data"
 
-
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
-
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
-
-    return redirect('/')
+    if not title_error and not post_error:
+        return render_template('blog.html', title=title, post=post)
+    else:
+        return render_template('newpost.html', title_error=title_error, 
+            post_error=post_error,
+            title=title,
+            post=post)
 
 
-if __name__ == '__main__':
-    app.run()
+# @app.route('/newpost', methods=['POST'])
+# def add_entry():
+
+#     blog_id = int(request.form['blog-id'])
+#     entry = Blog.query.get(blog_id)
+
+#     return redirect('/')
+
+
+# if __name__ == '__main__':
+app.run()
